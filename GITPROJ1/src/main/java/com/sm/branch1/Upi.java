e71a110153794f738be6e53adb874f18a1d855b3
ashok <ashok@DESKTOP-P66UJC3>
2025-11-14 12:46:08 +0530

    added upi

---
+++ GITPROJ1/src/main/java/com/sm/branch1/Upi.java
@@ -0,0 +1,12 @@
+package com.sm.branch1;
+
+import java.util.Random;
+
+public class Upi {
+
+	public static void main(String[] args) {
+		System.out.println("amount transfered"+new Random().nextInt()+1);
+
+	}
+
+}
